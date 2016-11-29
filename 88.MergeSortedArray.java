d7816d55c4389e8bce149159c71d331b2f1153c2
Xiao Liu <liu.xiao@me.com>
2016-11-29 21:41:21 +0000

    Add cleaner solution 88

--- 88.MergeSortedArray.java
+++ 88.MergeSortedArray.java
@@ -25,12 +25,37 @@ class MergeSortedArray {
     System.out.println(Arrays.toString(nums1));
   }
 
+  public void mergeStyled(int[] nums1, int m, int[] nums2, int n) {
+    int a = m - 1;
+    int b = n - 1;
+    int c = m + n - 1;
+    while (c >= 0) {
+      if (a >=0 && b >= 0) {
+        if (nums1[a] > nums2[b]) {
+          nums1[c] = nums1[a];
+          a--;
+        } else {
+          nums1[c] = nums2[b];
+          b--;
+        }
+      } else if (a >= 0) {
+        nums1[c] = nums1[a];
+        a--;
+      } else if (b >= 0) {
+        nums1[c] = nums2[b];
+        b--;
+      }
+      c--;
+    }
+    System.out.println(Arrays.toString(nums1));
+  }
+
   public static void main(String[] args) {
     int[] nums1 = new int[10];
     nums1[0] = 2;
     nums1[1] = 0;
     nums1[2] = 3;
     int[] nums2 = new int[] {1};
-    new MergeSortedArray().merge(nums1, 1, nums2, 1);
+    new MergeSortedArray().mergeStyled(nums1, 1, nums2, 1);
   }
 }
